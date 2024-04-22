35066b9d417a85267e364c43d34647d8ce769285
odavid10 <oder1011119504@gmail.com>
2024-04-21 23:15:47 -0400

    Pagina de bienvenida

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,51 @@
+"use client";
+
+import Link from "next/link";
+
 // material-ui
-import { Typography } from "@mui/material";
+import {
+  Typography,
+  Grid,
+  Button,
+  useTheme,
+  useMediaQuery,
+} from "@mui/material";
 
 // project imports
-import styles from "./page.module.css";
+import { RobotHi } from "./assets/illustration/RobotHi";
+import { RobotHiResponsive } from "./assets/illustration/RobottHiResponsive";
 
 export default function Home() {
+  const theme = useTheme();
+
   return (
-    <main className={styles.main}>
-      <Typography variant="h1">Home page</Typography>
-    </main>
+    <>
+      <Typography variant="h2" textAlign={"center"}>
+        Bienvenido
+      </Typography>
+      <Grid
+        container
+        mt={1}
+        spacing={6}
+        flexDirection={"column"}
+        justifyContent={"center"}
+        alignItems={"center"}
+      >
+        <Grid item xs={12}>
+          {useMediaQuery(theme.breakpoints.down("md")) ? (
+            <RobotHiResponsive />
+          ) : (
+            <RobotHi />
+          )}
+        </Grid>
+        <Grid item xs={12}>
+          <Link href="/routes" passHref>
+            <Button variant="contained" color="secondary">
+              Visita nuestras rutas
+            </Button>
+          </Link>
+        </Grid>
+      </Grid>
+    </>
   );
 }
