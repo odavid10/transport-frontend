6777e324d58f7f535fd16fed90aefea62428e9bf
odavid10 <oder1011119504@gmail.com>
2024-04-22 22:34:01 -0400

    update .envs

--- src/app/api/axiosClient.ts
+++ src/app/api/axiosClient.ts
@@ -1,5 +1,8 @@
 import axios from "axios";
 
+// project imports
+import { API_URL } from "../utils";
+
 export const axiosClient = axios.create({
-  baseURL: "http://localhost:4000/api/",
+  baseURL: API_URL,
 });
--- src/app/utils/constants.ts
+++ src/app/utils/constants.ts
@@ -1,3 +1,4 @@
 import jsonPackage from "../../../package.json" with {type:"json"};
 
 export const APP_VERSION: string = jsonPackage?.version || "0.0.1";
+export const API_URL = process.env.NEXT_PUBLIC_API_URL
